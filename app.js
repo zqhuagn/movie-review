88c7748b2d2db0fe9692e2225473bb36d13392da
zqhuagn <zqhuang@brandeis.edu>
2018-06-12 09:26:18 -0400

    MVP

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var writeRouter = require('./routes/write');
+var readRouter = require('./routes/read');
+var signupRouter = require('./routes/signup');
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/write', writeRouter);
+app.use('/read', readRouter);
+app.use('/signup', signupRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
