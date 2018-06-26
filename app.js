fc882e9d1233fe343d596fc40ed78acfe764e8b2
zqhuagn <zqhuang@brandeis.edu>
2018-06-26 10:03:29 -0400

    auth

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const
+  createError = require('http-errors'),
+  express = require('express'),
+  path = require('path'),
+  cookieParser = require('cookie-parser'),
+  logger = require('morgan'),
+  reviewsController = require('./controllers/reviewsController'),
+  usersController = require('./controllers/usersController'),
+  session = require("express-session"),
+  bodyParser = require("body-parser"),
+  User = require( './models/user' ),
+  flash = require('connect-flash')
 
+//var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy; //in the auth.js, so we do not need it here
+ // here we set up authentication with passport
+const passport = require('passport');
+const configPassport = require('./config/passport');
+configPassport(passport);
+
+/*
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var reviewRouter = require('./routes/review');
 var readRouter = require('./routes/read');
 var signupRouter = require('./routes/signup');
-
+*/
 var app = express();
 
 
-const reviewsController = require('./controllers/reviewsController')
 
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect( 'mongodb://localhost/moviereview' );
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -31,17 +44,110 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'zzbbyanana' }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// this handles all static routes ...
+// so don't name your routes so they conflict with the public folders
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.get('/review', reviewsController.getAllReviews );
-app.post('/saveReview', reviewsController.saveReview);
-app.post('/deleteReview', reviewsController.deleteReview);
-app.use('/users', usersRouter);
-//app.use('/review', reviewRouter);
-app.use('/read', readRouter);
-app.use('/signup', signupRouter);
+// here is where we check on their logged in status
+app.use((req,res,next) => {
+  res.locals.loggedIn = false
+  if (req.isAuthenticated()){
+    console.log("user has been Authenticated")
+    res.locals.user = req.user
+    res.locals.loggedIn = true
+    if (req.user){
+      if (req.user.googleemail=='zqhuang@brandeis.edu'){
+        console.log("Owner has logged in")
+        res.locals.status = 'teacher'
+      } else {
+        console.log('student has logged in')
+        res.locals.status = 'student'
+      }
+    }
+  }
+  next()
+})
 
+// add authentication routes
+// visit this route to start the google authentication
+// passport will send you to google to get auth
+// and then will send the browser back to /login/authorized page
+app.get('/loginerror', function(req,res){
+  res.render('loginerror',{})
+})
+
+app.get('/login', function(req,res){
+  res.render('login',{})
+})
+
+// route for logging out
+app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+
+  // GOOGLE ROUTES =======================
+
+app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+
+app.get('/login/authorized',
+        passport.authenticate('google', {
+                successRedirect : '/',
+                failureRedirect : '/loginerror'
+        }));
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+    console.log("checking to see if they are authenticated!")
+    // if user is authenticated in the session, carry on
+    res.locals.loggedIn = false
+    if (req.isAuthenticated()){
+      console.log("user has been Authenticated")
+      return next();
+    } else {
+      console.log("user has not been authenticated...")
+      res.redirect('/login');
+    }
+}
+
+// we require them to be logged in to see their profile
+app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+//app.use('/', indexRouter);
+app.get('/review', reviewsController.getAllReviews );
+app.post('/saveReview', isLoggedIn, reviewsController.saveReview);
+app.post('/deleteReview', isLoggedIn, reviewsController.deleteReview);
+
+app.get('/users',usersController.getAllUsers)
+app.get('/users/:id',
+        usersController.attachUser,
+        //reviewsController.attachReview,
+        usersController.getUser)
+//app.get('/reviewItem/:id',
+        // reviewsController.getReviewItem );
+//app.use('/users', usersRouter);
+//app.use('/review', reviewRouter);
+//app.use('/read', readRouter);
+//app.use('/signup', signupRouter);
+
+
+app.use('/', function(req, res, next) {
+  console.log("in / controller")
+  res.render('index', { title: 'Movie Review App' });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -9,8 +9,8 @@ exports.getAllReviews = ( req, res ) => {
   Review.find( {} )
     .exec()
     .then( ( theReviews ) => {
-      console.log('reviews are: ')
-      console.dir(theReviews)
+      //console.log('reviews are: ')
+      //console.dir(theReviews)
       res.render( 'review', {
         rs: theReviews
       } );
@@ -23,19 +23,52 @@ exports.getAllReviews = ( req, res ) => {
       console.log( 'review promise complete' );
     } );
 };
+/*
+exports.attachReview = ( req, res, next ) => {
+  console.log('in attachReviewss')
+  Skill.find( {student:res.locals.user.googleemail} )
+    .exec()
+    .then( ( reviews ) => {
+      res.locals.reviews = reviews
+      next()
+    } )
+    .catch( ( error ) => {
+      console.log( error.message );
+      return [];
+    } )
+    .then( () => {
+      console.log( 'review promise complete' );
+    } );
+};
 
+exports.getReviewItem = ( req, res, next ) => {
+  console.log('in getReviewItem')
+  const objId = new mongo.ObjectId(req.params.id)
+  Review.findOne(objId) //{"_id": objId})
+    .exec()
+    .then( ( review ) => {
+      res.render('reviewItem',{r:reviews})
+    } )
+    .catch( ( error ) => {
+      console.log( error.message );
+      return [];
+    } )
+    .then( () => {
+      console.log( 'attachOneReview promise complete' );
+    } );
+};
 
-
+*/
 
 exports.saveReview = ( req, res ) => {
-  console.log("in saveReview!")
-  console.dir(req)
+  //console.log("in saveReview!")
+  ///console.dir(req)
   let newReview = new Review( {
     headline: req.body.headline,
     content: req.body.content
   } )
 
-  console.log("review = "+newReview)
+  //console.log("review = "+newReview)
 
   newReview.save()
     .then( () => {
@@ -66,5 +99,5 @@ exports.deleteReview = (req, res) => {
     console.log("This shouldn't happen!")
     res.send(`unknown reviewName: ${reviewName}`)
   }
-``
+
 };
