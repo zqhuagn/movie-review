88a47dfccd2e7c7505513a8f30419610e918a0b0
zqhuagn <zqhuang@brandeis.edu>
2018-06-26 14:24:15 -0400

    auth

--- config/auth.js
+++ config/auth.js
@@ -3,14 +3,11 @@ module.exports = {
     'googleAuth' : {
         'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
         'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
-        'callbackURL'   : 'http://127.0.0.1:5000/login/authorized'
-    }
-
-    'googleAuth' : {
         'clientID': '845794852670-pis0vsec2buq51natdlsc2b2stqhhgvh.apps.googleusercontent.com',
         'clientSecret'  : 'Bng9c1UBkX4nWwSI_HKImVuj',
         'callbackURL'   : 'http://127.0.0.1:5000/login/authorized'
     }
+
 
 };
 //246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com
