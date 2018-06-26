b9061bc1d1c2060e5ba65b663177c74aa201ca37
zqhuagn <zqhuang@brandeis.edu>
2018-06-26 14:31:35 -0400

    auth

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,8 @@
 module.exports = {
 
     'googleAuth' : {
-        'clientID': '845794852670-pis0vsec2buq51natdlsc2b2stqhhgvh.apps.googleusercontent.com',
-        'clientSecret'  : 'Bng9c1UBkX4nWwSI_HKImVuj',
-
         'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
         'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
-        
         'callbackURL'   : 'http://127.0.0.1:5000/login/authorized'
     }
 
