05fe1789cf2b59c58184949eba7583df3604508a
zqhuagn <zqhuang@brandeis.edu>
2018-07-01 15:03:08 -0400

    delete s

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    'googleAuth' : {
+        'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
+        'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
+        'callbackURL'   : 'http://127.0.0.1:5000/login/authorized'
+    }
+
+
+};
+//246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com
+//iqgLqu6pXgLuHsZFq6nvxDX3
+
+//845794852670-f0v48f14a40kqrn6gfn4ibuhni7nljq6.apps.googleusercontent.com
+//7nbSlzMB3WNnqlYUeuOh-pjQ
