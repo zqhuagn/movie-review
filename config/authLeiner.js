4dc55ce567759724b4f36a469f91cada001ec6ce
zqhuagn <zqhuang@brandeis.edu>
2018-07-01 21:20:00 -0400

    changeauth

--- config/authLeiner.js
+++ config/authLeiner.js
@@ -1,8 +1,8 @@
 module.exports = {
 
     'googleAuth' : {
-        'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
-        'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
+        'clientID': '845794852670-e6pcslhrtl46la0t9dcn1l04irpgpfgq.apps.googleusercontent.com',
+        'clientSecret'  : '2c78sbKhO6QnMCPkDUuFes9u',
         'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6600/login/authorized'
     }
 
